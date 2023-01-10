c51e47963499bb5a6c9c471a70233c39afaa30bb
gabrielaabergamo <gabiibergamo@gmail.com>
2023-01-09 22:16:25 -0300

    new page (registry)

---
+++ app/src/pages/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <page-wrapper>
+    <div class="register">
+      <h2 class="register-title">Cadastre sua empresa</h2>
+      <hr class="register-divider" />
+    </div>
+  </page-wrapper>
+</template>
+
+<script>
+import PageWrapper from './PageWrapper.vue';
+
+export default {
+  name: 'RegisterPage',
+  components: {
+    PageWrapper,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.register {
+  min-height: 100vh;
+  background-color: #fef4ea;
+  padding: 40px 60px;
+
+  &-title {
+    font-weight: normal;
+    color: #595959;
+    margin-bottom: 12px;
+  }
+  &-divider {
+    width: 105px;
+    margin: 0;
+  }
+}
+</style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
-import SearchPage from "../pages/Search"
+import { createRouter, createWebHistory } from 'vue-router';
+import SearchPage from '../pages/Search';
+import RegisterPage from '../pages/Register';
 
 const routes = [
-    {
-        path: "/",
-        name: "Main",
-        component: SearchPage,
-      },
-    {
-        path: "/search",
-        name: "Search",
-        component: SearchPage
-    },
-]
+  {
+    path: '/',
+    name: 'Main',
+    component: SearchPage,
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: SearchPage,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  
-  export default router;
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
