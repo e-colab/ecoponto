4bee2ae375edefb7d69e57f1136a05d0b981914e
Arthur <arthur.fernando03@gmail.com>
2023-02-10 11:35:45 -0300

    post Material Service

--- app/src/service/MaterialService.js
+++ app/src/service/MaterialService.js
@@ -8,5 +8,23 @@ export default {
         } catch(err) {
             console.log(err)
         }
+    },
+
+    async postMateriais(nome, qualidade, unidade, categoria, cnpj, objetivo, quantidade, medidaCadastrada) {
+        try {
+            const response = await Api().post('/cadastrar-material', {
+                nome: nome,
+                qualidade: qualidade,
+                unidade: unidade,
+                categoria: categoria,
+                cnpj: cnpj,
+                objetivo: objetivo,
+                quantidade: quantidade,
+                medidaCadastrada: medidaCadastrada
+            })
+            return response.data
+        } catch(err) {
+            console.log(err)
+        }
     }
 }
