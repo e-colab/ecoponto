57a6a4429a70eb959c21f43e92a61cf0d4058fa2
Arthur <arthur.fernando03@gmail.com>
2023-02-18 18:58:57 -0300

    remoção de comentario

--- api/database/dbConfig.js
+++ api/database/dbConfig.js
@@ -1,20 +1,4 @@
-// const Sequelize = require('sequelize')
 require('dotenv').config()
-
-
-// const sequelize = new Sequelize(
-//   process.env.DB_DATABASE,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     dialect: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     schema: process.env.DB_SCHEMA
-//   }
-// )
-
-// module.exports = sequelize
 
 const { Pool } = require('pg')
 const pool = new Pool({
