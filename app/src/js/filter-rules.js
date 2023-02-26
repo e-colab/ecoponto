84d45584ca6df148b6be4463205a37676e526f0b
gabrielaabergamo <gabiibergamo@gmail.com>
2023-02-26 17:32:00 -0300

    simplyfing the filter functions

--- app/src/js/filter-rules.js
+++ app/src/js/filter-rules.js
@@ -22,56 +22,17 @@ export default function filterElements(
     filterArray = filterQuality(filterArray, quality);
   }
 
-  // if (volume.measure && volume.value) {
-  //   filterArray = filterVolume(filterArray, volume);
-  // }
   return filterArray;
 }
 
 function filterMaterial(array, materials) {
-  const filteredArray = array.filter((item) => {
-    for (let i = 0; i < materials.length; i++) {
-      if (item.descricao === materials[i]) {
-        return true;
-      }
-    }
-    return false;
-  });
-  return filteredArray;
+  return array.filter((item) => materials.includes(item.descricao));
 }
 
 function filterObjective(array, objective) {
-  const filteredArray = array.filter((item) => {
-    for (let i = 0; i < objective.length; i++) {
-      if (item.objetivo === objective[i]) {
-        return true;
-      }
-    }
-    return false;
-  });
-  return filteredArray;
+  return array.filter((item) => objective.includes(item.objetivo));
 }
 
 function filterQuality(array, quality) {
-  const filteredArray = array.filter((item) => {
-    for (let i = 0; i < quality.length; i++) {
-      if (item.qualidade === quality[i]) {
-        return true;
-      }
-    }
-    return false;
-  });
-  return filteredArray;
+  return array.filter((item) => quality.includes(item.qualidade));
 }
-
-// function filterVolume(array, volume) {
-//   const filteredArray = array.filter((item) => {
-//     if (item.unidade === volume.measure) {
-//       if (item.quantidade >= volume.value) {
-//         return true;
-//       }
-//     }
-//     return false;
-//   });
-//   return filteredArray;
-// }
