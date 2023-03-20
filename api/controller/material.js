de605dd7981dddfd66b68a1a46ec299f2810298f
Arthur <arthur.fernando03@gmail.com>
2023-03-20 20:47:25 -0300

    add function to filter materials

--- api/controller/material.js
+++ api/controller/material.js
@@ -1,12 +1,137 @@
+const { query } = require('express')
 const pool = require('../database/dbConfig')
 
 
-exports.getMaterial = (req, res, next) => {
+function addValues(arr, val, filterStr) {
+    len = val.length
+    filterStr += '('
+    for(i in val) {
+        arr.push(val[i])
+        filterStr += '\''+ val[i] + '\''
+        if(i < len-1) {
+            filterStr += ', '
+        }
+    }
+    filterStr += ') '
 
-    pool.query('SELECT M.nome as MaterialNome, M.qualidade, EM.data, C.descricao, E.cnpj, E.nome as EmpresaNome, E.email, E.telefone, E.funcResponsavel, E.cep, E.cidade, E.estado, E.endereco, E.bairro, E.numeroEndereco, E.lat, E.long, EM.objetivo FROM ecoponto.material M JOIN ecoponto.empresamaterial EM ON M.idprod = EM.idprod JOIN ecoponto.empresa E ON E.cnpj = EM.cnpj JOIN ecoponto.categoria C ON C.idCategoria = M.categoria')
+    return filterStr
+}
+
+exports.getMateriais = (req, res, next) => {
+
+    pool.query(`
+        SELECT
+            M.nome as MaterialNome,
+            M.qualidade,
+            EM.data,
+            C.descricao,
+            E.cnpj,
+            E.nome as EmpresaNome,
+            E.email,
+            E.telefone,
+            E.funcResponsavel,
+            E.cep,
+            E.cidade,
+            E.estado,
+            E.endereco,
+            E.bairro,
+            E.numeroEndereco,
+            E.lat,
+            E.long,
+            EM.objetivo
+        FROM ecoponto.material M
+        JOIN ecoponto.empresamaterial EM
+            ON M.idprod = EM.idprod
+        JOIN ecoponto.empresa E
+            ON E.cnpj = EM.cnpj
+        JOIN ecoponto.categoria C
+            ON C.idCategoria = M.categoria
+    `)
     .then(material => {
         console.log(material.rows)
         res.send(material.rows)
+    })
+    .catch(err => {
+        console.error(err.stack)
+    })
+}
+
+exports.retrieveMaterial = (req, res, next) => {
+    const qualidade = req.body.qualidade
+    const objetivo = req.body.objetivo
+    const categoria = req.body.categoria
+    var values = []
+    var filterValues = "WHERE "
+    var hasVal = true
+
+    if(categoria.length > 0) {
+        filterValues += addValues(values, categoria, 'C.nomecategoria IN ')
+    } else {
+        hasVal = false
+    }
+
+    if(qualidade.length > 0) {
+        if(hasVal) {
+            filterValues += 'AND '
+        }
+        filterValues += addValues(values, qualidade, 'M.qualidade IN ')
+        hasVal = true
+    } else {
+        hasVal = false
+    }
+
+    if(objetivo.length > 0) {
+        if(hasVal) {
+            filterValues += 'AND '
+        }
+        filterValues += addValues(values, objetivo, 'EM.objetivo IN ')
+    }
+
+    const selectMat = 'SELECT M.nome as MaterialNome, M.qualidade, EM.data, M.idprod, C.descricao, E.cnpj, E.nome as EmpresaNome, E.email, E.telefone, E.funcResponsavel, E.cep, E.cidade, E.estado, E.endereco, E.bairro, E.numeroEndereco, E.lat, E.long, EM.objetivo FROM ecoponto.material M JOIN ecoponto.empresamaterial EM ON M.idprod = EM.idprod JOIN ecoponto.empresa E ON E.cnpj = EM.cnpj JOIN ecoponto.categoria C ON C.idCategoria = M.categoria '
+    const queryMat = selectMat + filterValues
+
+    pool.query(queryMat)
+    .then(material => {
+        
+        var unique = [...new Set(material.rows.map((item) => item.cnpj))]
+        var filteredMat = []
+        unique.forEach(id => {
+            var objeto = {}
+            objeto.materiais = []
+
+            material.rows.forEach(data => {
+                if(id === data.cnpj) {
+                    if(!Object.hasOwn(objeto, 'cnpj')) {
+                        objeto.cnpj = data.cnpj
+                    }
+                    objeto.empresanome = data.empresanome
+                    objeto.email = data.email
+                    objeto.funcresponsavel = data.funcresponsavel
+                    objeto.telefone = data.telefone
+                    objeto.cep = data.cep
+                    objeto.cidade = data.cidade
+                    objeto.estado = data.estado
+                    objeto.endereco = data.endereco
+                    objeto.bairro = data.bairro
+                    objeto.numeroendereco = data.numeroendereco
+                    objeto.lat = data.lat
+                    objeto.long = data.long
+
+                    var matObj = {}
+                    matObj.idprod = data.idprod
+                    matObj.materialnome = data.materialnome
+                    matObj.descricao = data.descricao
+                    matObj.qualidade = data.qualidade
+                    matObj.objetivo = data.objetivo
+                    matObj.data = data.data.toDateString()
+
+                    objeto.materiais.push(matObj)
+                }
+            })
+            filteredMat.push(objeto)
+        })
+
+        res.send(filteredMat)
     })
     .catch(err => {
         console.error(err.stack)
