b6d0b6ca1c40cdc0a773b53c83aba859577c3edd
Arthur <arthur.fernando03@gmail.com>
2023-03-23 13:40:11 -0300

    fix insert material

--- api/controller/material.js
+++ api/controller/material.js
@@ -104,16 +104,18 @@ exports.retrieveMaterial = (req, res, next) => {
 }
 
 exports.postMaterial = async (req, res, next) => {
-    const arr = req.body.array
-    for(i in arr) {
-        const qualidade = arr[i].quality
-        const nome = arr[i].name
-        const categoria = arr[i].material
-        const cnpj = arr[i].companyID
-        const objetivo = arr[i].objective
+    const arr = Object.entries(req.body.array[0])
+
+    for(i in arr.slice(0,-1)) {
+        const qualidade = arr.at(i).at(1).quality
+        const nome = arr.at(i).at(1).name
+        const categoria = arr.at(i).at(1).material
+        const cnpj = arr.at(-1).at(-1)
+        const objetivo = arr.at(i).at(1).objective
 
         const selectFields = 'SELECT * FROM ecoponto.material M, ecoponto.empresa E WHERE E.cnpj = $1 AND M.qualidade = $2 AND M.nome = $3'
         const selectCat = `SELECT idCategoria FROM ecoponto.categoria C WHERE C.descricao = '${categoria}'`
+
         const insertMaterial = 'INSERT INTO ecoponto.material(qualidade, nome, categoria) VALUES ($1, $2, $3) RETURNING idProd'
         const insertEmpresaMaterial = 'INSERT INTO ecoponto.empresamaterial(cnpj, idProd, qualidade, objetivo, categoria) VALUES ($1, $2, $3, $4, $5)'
 
@@ -123,11 +125,11 @@ exports.postMaterial = async (req, res, next) => {
         
         try {
             await client.query('BEGIN')
-            console.log(selectCat)
             const queryFields = await client.query(selectFields, values)
             
             if(queryFields.rows.length === 0) {
                 const queryCat = await client.query(selectCat)
+
                 const valuesMaterial = [qualidade, nome, queryCat.rows[0].idcategoria]
                 const queryMat = await client.query(insertMaterial, valuesMaterial)
 
