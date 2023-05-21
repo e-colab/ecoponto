443559947ffb086207ee738e2144cee86cdd96ca
gabrielaabergamo <gabiibergamo@gmail.com>
2023-05-21 20:33:50 -0300

    added ODS images

--- app/src/pages/About.vue
+++ app/src/pages/About.vue
@@ -3,11 +3,21 @@
         <section class="about">
             <Title title="Sobre nós"/>
             <p class="about-text">
-                O e-colab Empresas é uma plataforma web que assessora a <span class="about-text__highlight">gestão dos resíduos sólidos</span> promovendo a sua destinação adequada, incentivando a Economia Circular, através da Logística Reversa. O intuito é permitir o cadastro de empresas e facilitar a interação entre aquelas interessadas em comercializar (compra e venda), doar e/ou coletar os resíduos de produtos/matérias primas.
-                <br />
-                <br />
+                O e-colab Empresas é uma plataforma web que assessora a <span class="about-text__highlight">gestão dos resíduos sólidos</span> promovendo a sua destinação adequada, incentivando a Economia Circular, através da Logística Reversa. O intuito é permitir o cadastro de empresas e facilitar a interação entre aquelas interessadas em comercializar (compra e venda), doar e/ou coletar os resíduos de produtos/matérias primas. 
+            </p>
+
+            <p class="about-text">
                 Pertencentes ao <span class="about-text__highlight">Centro de Ciências em Gestão e Tecnologia</span> (CCGT) da Universidade Federal de São Carlos - campus Sorocaba, o grupo reúne pesquisadores e estudantes de diferentes formações e áreas de conhecimento, interessados em proteger o meio ambiente, a partir da disseminação da informação, de ações educativas e de orientação à comunidade em geral alinhadas aos objetivos de desenvolvimento sustentável da ONU. O nosso grupo tem como propósito específico o estudo e desenvolvimento de métodos e práticas para promover a Logística Reversa e a Economia Circular. 
             </p>
+
+            <p class="about-text">
+                Nosso projeto contribue, diretamente, com os seguintes ODS:
+            </p>
+            <div class="about-images">
+                <img src="../assets/ods-11.png" />
+                <img src="../assets/ods-12.png" />
+                <img src="../assets/ods-15.png" />
+            </div>
         </section>
     </page-wrapper>
 </template>
@@ -35,10 +45,19 @@ export default {
         font-size: 1.5em;
         letter-spacing: .03rem;
         color: #404040;
+        margin-bottom: 15px;
 
         &__highlight{
             background-image:linear-gradient( to bottom, transparent 55%, #c3d2c1 35%);
         }
+    }
+
+    &-images{
+        display: flex;
+        justify-content: center;
+        gap: 100px;
+        margin-top: 40px;
+        flex-wrap: wrap;
     }
 }
 </style>
