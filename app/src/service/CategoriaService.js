24b334146d22a762281221fc11985b3eb8126966
Arthur <arthur.fernando03@gmail.com>
2023-07-09 21:14:16 -0300

    update Categoria controller & add Categoria service

---
+++ app/src/service/CategoriaService.js
@@ -0,0 +1,24 @@
+import Api from './api'
+
+export default {
+    async getCategorias() {
+        try {
+            const response = await Api().get('/categorias')
+            return response.data
+        } catch(err) {
+            console.log(err)
+        }
+    },
+
+    async postCategoria(payload) {
+        try {
+            const response = await Api().post('/cadastrar-categoria', {
+                nomeCategoria: payload.nomeCategoria,
+                descricao: payload.descricao
+            })
+            return response.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+}
