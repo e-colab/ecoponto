b19f5df002f4cad0c484d4ce50e0912535a0fd6c
Arthur <arthur.fernando03@gmail.com>
2023-07-23 20:36:36 -0300

    add qualide service

---
+++ app/src/service/QualidadeService.js
@@ -0,0 +1,24 @@
+import Api from './api'
+
+export default {
+    async getQualidade() {
+        try {
+            const response = await Api().get('/qualidades')
+            console.log('data', response.data)
+            return response.data
+        } catch(err) {
+            console.log(err)
+        }
+    },
+
+    async postCategoria(payload) {
+        try {
+            const response = await Api().post('/cadastrar-qualidade', {
+                descricao: payload.descricao
+            })
+            return response.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+}
