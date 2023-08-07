a683f63681de8833747dc558c433e0b602a5ef87
gabrielaabergamo <gabiibergamo@gmail.com>
2023-08-06 22:24:36 -0300

    contact page

---
+++ app/src/pages/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+    <page-wrapper>
+        <section class="contact">
+            <Title title="Contato"/>
+            <p>Entre em contato através do e-mail: e-colab@ufscar.br</p>
+        </section>
+    </page-wrapper>
+</template>
+
+<script>
+import PageWrapper from './PageWrapper.vue';
+import Title from '../components/common/title.vue';
+
+export default {
+    name: 'ContactPage',
+    components: {
+        PageWrapper,
+        Title
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.contact{
+    background-color: #fef4ea;
+    min-height: 100vh;
+    padding: 40px 80px;
+}
+</style>
